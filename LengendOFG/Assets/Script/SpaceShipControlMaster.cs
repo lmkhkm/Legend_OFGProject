39433d2af50668bcaded211541f579231d4b340d
HeoJunKu <lmkhkm@naver.com>
2020-02-18 15:42:13 +0900

    SpaceShipCanvas 추가
    SpaceShipCanvas를 추가하고 캔버스에 들어갈 이미지 위치를 잡음

---
+++ LengendOFG/Assets/Script/SpaceShipControlMaster.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShipControlMaster : MonoBehaviour
+{
+    public Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetMainCamera();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetMainCamera()
+    {
+        mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        gameObject.transform.Find("SpaceShipCanvas").gameObject.GetComponent<Canvas>().worldCamera = mainCamera;
+        return;
+    }
+}
