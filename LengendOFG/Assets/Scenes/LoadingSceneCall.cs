9c5249e7f2ac5d69dc98b95ae3ba24d546bc13fa
HeoJunKu <lmkhkm@naver.com>
2020-03-13 00:27:37 +0900

    로딩 씬을 이용한 씬 이동 구현 완료
    로딩씬을 이용한 씬 이동 코드를 작성 완료하고 적용함

---
+++ LengendOFG/Assets/Scenes/LoadingSceneCall.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingSceneCall : MonoBehaviour
+{
+
+    public static string nextScene;
+    public Slider loadingSlider;
+    public Text percentText;
+
+    private void Start()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    public static void LoadScene(string sceneName)
+    {
+        nextScene = sceneName;
+        SceneManager.LoadScene("LoadingScene");
+    }
+    
+    IEnumerator LoadScene()
+    {
+        yield return null;
+
+        AsyncOperation asyncOp = SceneManager.LoadSceneAsync(nextScene);
+        asyncOp.allowSceneActivation = false;
+
+        while(!asyncOp.isDone)
+        {
+            yield return null;
+
+
+            if (loadingSlider.value < 0.9f)
+            {
+                loadingSlider.value = asyncOp.progress;
+                percentText.text = (asyncOp.progress * 100) + " " + "%";
+            }
+            else
+            {
+                loadingSlider.value += 0.01f;
+                yield return new WaitForSeconds(0.05f);
+                percentText.text = (loadingSlider.value * 100f).ToString() + " " + "%";
+
+                if (loadingSlider.value == 1.0f)
+                {
+                    percentText.text = "100%";
+                    yield return new WaitForSeconds(0.5f);
+                    asyncOp.allowSceneActivation = true;
+                    yield break;
+                }
+            }
+
+        }
+    }
+
+}
---
+++ LengendOFG/Assets/Script/SceneScript/SceneLoad.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void SceneLoadByLoading(string sceneName)
+    {
+        LoadingSceneCall.LoadScene(sceneName);
+
+    }
+}
