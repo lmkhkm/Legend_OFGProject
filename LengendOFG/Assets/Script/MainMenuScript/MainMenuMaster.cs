ca1babc0e7d0032983c9a2299bbf98a9d20331c8
HeoJunKu <lmkhkm@naver.com>
2020-03-12 23:29:22 +0900

    메인 메뉴 기본 버튼 스크립트 연결

---
+++ LengendOFG/Assets/Script/MainMenuScript/MainMenuMaster.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuMaster : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject mainMenu;
+    public GameObject newGame;
+    public GameObject loadGame;
+    public GameObject setting;
+    
+
+    public void ChangeMainMenuWindow(GameObject mainMenuWindow)
+    {
+        mainMenuWindow.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+
+    public void GoMainMenuWindow(GameObject mainMenuWindow)
+    {
+        mainMenuWindow.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+}
--- LengendOFG/Assets/Script/SceneScript/LoadingSceneCall.cs
+++ LengendOFG/Assets/Script/SceneScript/LoadingSceneCall.cs
@@ -1,18 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LoadingSceneCall : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public static string nextScene;
+
+    public Image progressBar;
+
+
+    AsyncOperation ansyncOp;
 }
